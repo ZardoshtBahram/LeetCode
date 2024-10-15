f071b68629664651c80cc4faf207bca697e4d835
Bahram Zardosht <zardoshtbahram81@gmail.com>
2024-10-15 15:37:20 -0400

    Add project files.

---
+++ Leetcode_C#/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Leetcode_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+  
+        public long MinimumSteps(string s)
+        {
+            long sawp = 0;
+            long blockofones = 0;
+
+            foreach (char item in s)
+            {
+                if (item == '0')
+                    sawp += blockofones;
+                
+                else
+                    blockofones++;
+            }
+
+            return sawp;
+        }
+    }
+}
